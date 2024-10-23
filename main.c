c0ef08f9cd7f24417b455ad836ef8289895b6780
Le Kha Vy <vy210473@student.nctu.edu.vn>
2024-10-23 10:16:48 +0700

    cau10

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Khai báo prototype của các hàm từ shellc1.c và shellc2.c
+void shellc1();
+void shellc2();
+
+int main() {
+    shellc1();
+    shellc2();
+    return 0;
+}
